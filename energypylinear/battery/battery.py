58c10f8a80a436a330fa45142b31f3158c4523e7
Luke Marshall <lukasmarshall@gmail.com>
2021-01-19 14:08:12 +1100

    Added new optimisation function

--- energypylinear/battery/battery.py
+++ energypylinear/battery/battery.py
@@ -205,7 +205,7 @@ class Battery(object):
 
 
         self.prob += lpSum(
-            [ abs(forecasts[i] - (imports[i] + exports[i]))  for i in idx[:-1] ]
+            [ (forecasts[i] - (imports[i] + exports[i]))**2  for i in idx[:-1] ]
         )
 
         #  initial charge
